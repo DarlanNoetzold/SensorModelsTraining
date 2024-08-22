6179b58fc129513b2a1cf4514dba07cfb886928c
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-08-21 23:16:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,31 @@ from data_preprocessing import load_data, preprocess_data, create_preprocessing_
 from model_training import train_models
 from model_evaluation import evaluate_models
 
+def remove_sequential_duplicates(df, ignore_columns):
+    """
+    Remove linhas consecutivas que têm os mesmos valores em todas as colunas,
+    exceto nas colunas especificadas em ignore_columns.
+    """
+    df_subset = df.drop(columns=ignore_columns)
+
+    df_shifted = df_subset.shift(1)
+
+    mask = (df_subset != df_shifted).any(axis=1)
+
+    df_cleaned = df[mask]
+
+    return df_cleaned
 
 def main():
     os.makedirs('models', exist_ok=True)
 
-    # Carregando os dados
     df = load_data()
     print(df.columns)
-    # Pré-processamento dos dados
+
+    ignore_columns = ["ID", "CPU Usage", "Memory Usage", "Thread Count", "Error Count"]
+
+    df = remove_sequential_duplicates(df, ignore_columns)
+
     df = preprocess_data(df)
 
     print(df.columns)
@@ -24,22 +41,17 @@ def main():
         print(f"\nTreinando e avaliando para target: {target}")
         X_train, X_test, y_train, y_test = split_data(df, target)
 
-        # Identificando as colunas numéricas e categóricas no X_train
         numeric_features = X_train.select_dtypes(include=['int64', 'float64']).columns
         categorical_features = X_train.select_dtypes(include=['object']).columns
 
-        # Criando o pipeline de pré-processamento com base nas colunas presentes
         preprocessor = create_preprocessing_pipeline(numeric_features, categorical_features)
 
-        # Aplicando o pré-processamento
         print(f"Aplicando pré-processamento para target: {target}")
         X_train = preprocessor.fit_transform(X_train)
         X_test = preprocessor.transform(X_test)
 
-        # Treinando os modelos
         models = train_models(X_train, y_train, target)
 
-        # Avaliando os modelos
         evaluate_models(models, X_test, y_test)
         print("\n" + "-" * 50 + "\n")
 
